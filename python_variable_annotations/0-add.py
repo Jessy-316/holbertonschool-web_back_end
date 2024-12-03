2fe7f84202050f5ea61a8b0ba92506dda8b7b1b3
franciscobaez <jessiebaez@outlook.com>
2024-12-03 13:50:22 -0400

    task 0 attempt 1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Type annotated function(add) that takes two float arguments (a, b),
+    and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two floating-point numbers """
+    return a + b
