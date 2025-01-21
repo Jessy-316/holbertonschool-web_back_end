9815304f306897681713d0e1d0d4a01201283a83
Francisco Antonio Baez Lopez <jessiebaez316@outlook.com>
2025-01-21 11:42:51 -0400

    task 1 attempt

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,12 +3,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null) {
-        process.stdout.write(`Your name is: ${name.trim()}\n`);
-    }
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name.trim()}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
