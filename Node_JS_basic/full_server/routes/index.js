7d65ee5f0a8d438444f13059dd424c7ce058e6bd
Francisco Antonio Baez Lopez <jessiebaez316@outlook.com>
2025-01-23 13:37:48 -0400

    changes made

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -6,16 +6,16 @@ function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
 
-  router.get('/', (request, resolve) => {
-    AppController.getHomepage(request, response);
+  router.get('/', (req, res) => {
+    AppController.getHomepage(req, res);
   });
 
-  router.get('/students', (request, resolve) => {
-    StudentsController.getAllStudents(request, response, process.argv[2]);
+  router.get('/students', (req, res) => {
+    StudentsController.getAllStudents(req, res, process.argv[2]);
   });
 
-  router.get('/students/:major', (request, response) => {
-    StudentsController.getAllStudentsByMajor(request, response, process.argv[2]);
+  router.get('/students/:major', (req, res) => {
+    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
   });
 }
 
